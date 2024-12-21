e3d33866392868a34fd9eabc8df74528aaa88cb2
sam damazio <samuel_damazio@hotmail.com>
2024-12-21 20:29:04 -0300

    x

--- aliado_backend/aliado_backend/asgi.py
+++ aliado_backend/aliado_backend/asgi.py
@@ -2,7 +2,7 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from game.routing import websocket_urlpatterns
+from game.routing import websocket_urlpatterns  # Importando as rotas WebSocket
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aliado_backend.settings')
 
@@ -10,7 +10,7 @@ application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            websocket_urlpatterns
+            websocket_urlpatterns  # Certifique-se de configurar corretamente essas rotas
         )
     ),
 })
